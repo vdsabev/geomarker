96f700b6b5292e54b1294e87baaa54e887d6e574
Vladimir Sabev <vdsabev@gmail.com>
2016-12-17 12:43:40 +0200

    Updated packages; added API key

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path');
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
-var app = express();
+const app = express();
 
-app.configure(function () {
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.favicon(__dirname + '/public/images/logo.png'));
-  app.use(express.logger('dev'));
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000);
+app.use(require('serve-favicon')(path.join(__dirname, 'public/images/logo.png')));
+app.use(require('morgan')('dev'));
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(require('serve-static')(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(require('errorhandler')());
+}
+
+// Pages
+app.get('/', (req, res) => {
+  res.sendFile('app.html', { root: __dirname });
 });
 
-app.configure('development', function () {
-  app.use(express.errorHandler());
-});
-
-//Pages
-app.get('/', function (req, res) {
-  res.sendfile('app.html');
-});
-
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), () => {
+  console.log(`Express server listening on port ${app.get('port')}`);
 });
